b944eb650dfa874358ef69667bdd065a12285b62
mk <minhkienoffcial@gmail.com>
2025-09-13 21:51:42 +0700

    Create event market.lua

---
+++ event market.lua
@@ -0,0 +1,39 @@
+return {
+    AUTO_RESTART_WHEN_UPDATE = true,
+    AUTO_ACHIEVEMENTS = false,
+    CRAFT_ITEMS = {},
+
+    AUTO_GAREN_FOR_EVENT = true,
+
+    BUY_SEED_SHOP = {"All"},
+    BUY_GEAR_SHOP = {"Trading Ticket", "Trowel", "Recall Wrench", "Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Master Sprinkler", "Grandmaster Sprinkler", "Medium Toy", "Medium Treat", "Levelup Lollipop"},
+    BUY_EGG_SHOP = {"All"},
+    BUY_EVENT_SHOP = {"All"},
+    BUY_MERCHANT_SHOP = {"All"},
+    BUY_HONEY_MERCHANT = false,
+
+	PLACE_SPRINKLERS = {"Basic Sprinkler", "Advanced Sprinkler", "Godly Sprinkler", "Master Sprinkler", "Grandmaster Sprinkler"},
+	PLACE_EGGS = {"Fall Egg"},
+	EQUIP_COSMETICS = {["Cooking Kit"] = 8},
+	EQUIP_PETS = {["Rooster"] = 8, ["Sunny-Side Chicken"] = 8, ["Chicken"] = 8},
+
+	LOCK_PET_NAME = {"Mimic Octopus", "Butterfly", "Dilophosaurus"},
+	LOCK_PET_RARITY = {"Divine", "Prismatic"},
+	LOCK_PET_COUNT = {["Sunny-Side Chicken"] = 8, ["Rooster"] = 8},
+	LOCK_PET_WEIGHT = 5,
+	LOCK_PET_AGE = 75,
+	EXTRA_PET_SLOT = 8,
+	EXTRA_EGG_SLOT = 8,
+
+    LIST_USERNAME = {},
+    GIFT_PET_NAME = {"Mimic Octopus", "Butterfly", "Dilophosaurus"},
+    GIFT_PET_RARITY = {"Divine", "Prismatic"},
+    ACCEPT_GIFT = true,
+    SHUTDOWN_AFTER_GIFT_PET = true,
+
+	NOTIFY_PET_NAME = {"Mimic Octopus", "Butterfly", "Dilophosaurus"},
+	NOTIFY_PET_RARITY = {"Divine", "Prismatic"},
+	NOTIFY_PET_WEIGHT = 5,
+	WEBHOOK_URL = "",
+	WEBHOOK_NOTE = "",
+}
